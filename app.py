a69513a6ed344f08d7a7e4f8214866de75ff1568
Henry Pickersgill <htap2@live.co.uk>
2024-02-08 19:25:45 +0000

    update requirements and imports

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 import dash
 import dash_bootstrap_components as dbc
-import html
 
 app = dash.Dash(__name__)
 server = app.server
 
 def update_layout():
     body = dbc.Container([
-        html.Div("Hello")
+        dash.html.Div("Hello")
     ])
     return body
 
