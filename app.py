e68d45501d273308dd2c3ab80cfe2421cb4bcc46
Henry Pickersgill <htap2@live.co.uk>
2024-02-08 19:17:46 +0000

    test adding layout to app

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 import dash
+import dash_bootstrap_components as dbc
+import html
 
 app = dash.Dash(__name__)
 server = app.server
+
+def update_layout():
+    body = dbc.Container([
+        html.Div("Hello")
+    ])
+    return body
+
+app.layout = update_layout()
